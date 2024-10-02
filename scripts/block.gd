57cfaf2968c0ae021c4e82ae8e054c7d1471de42
Aleksandr Menshikov <menshikov.abz@gmail.com>
2024-10-02 18:49:50 +0200

    update color by click

--- scripts/block.gd
+++ scripts/block.gd
@@ -5,14 +5,14 @@ extends Area2D
 
 var color_number
 var color_map = {
-	0: "red",
-	1: "black",
-	2: "blue"
+	1: "red",
+	2: "black",
+	3: "blue"
 }
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	color_number = range(len(color_map)).pick_random()
+	color_number = range(1, len(color_map) + 1).pick_random()
 	set_size(block_width, block_height)
 	update_color()
 
@@ -28,3 +28,13 @@ func update_color():
 	$Sprite2D.set_self_modulate(Color(color_map[color_number]))
 
 
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.is_pressed():
+		if color_number == len(color_map):
+			color_number = 1
+		else:
+			color_number += 1
+		print(color_number)
+		update_color()
